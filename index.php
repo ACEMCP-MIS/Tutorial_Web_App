497343a4a69177d5b0d5a7bf0bb78a3c72e381d7
Humphrey Castro <54143005+Preyyy@users.noreply.github.com>
2024-01-17 15:20:23 +0800

    Starting
    Created an interface

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ACEMCP - GUIDES</title>
+  <link rel="shortcut icon" href="icons/favicon.ico">
+  <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+
+
+
+<body>
+  
+<h1>ACE MEDICAL CENTER PALAWAN SYSTEM GUIDELINES</H1>
+  <section class="content demo">
+    
+    <div class="card-hghlght demo-card">
+      <div class="card-body m-5">
+        <p>HIS</p>
+      </div>
+    </div>
+    <div class="card-hghlght demo-card"></div>
+    <div class="card-hghlght demo-card"></div>
+    <div class="card-hghlght demo-card"></div>     
+  </section>
+</body>
+<FOOTer><p> quick quidelines</p></FOOTer>
+</html>
