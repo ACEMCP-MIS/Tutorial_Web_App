7bd31e190273a2919ea7abc206f21ccf85439699
Humphrey Castro <54143005+Preyyy@users.noreply.github.com>
2024-01-18 17:04:43 +0800

    Update Home_HIS.php

--- HIS/Home_HIS.php
+++ HIS/Home_HIS.php
@@ -5,21 +5,42 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ACEMCP - GUIDES</title>
-  <link rel="shortcut icon" href="icons/favicon.ico">
+  <link rel="shortcut icon" href="../icons/favicon.ico">
   <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <h1>HOSPITAL INFORMATION SYSTEM GUIDELINES</h1>
-    <div class="row g-0">
- 
-    <div class="col-6 col-md-4">
-      .col-6 .col-md-
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Content1" aria-expanded="false" aria-controls="collapseExample">
+    EMERGENCY
+  </button> 
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Content1" aria-expanded="false" aria-controls="collapseExample">
+    OUTPATIENT
+  </button> 
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Content1" aria-expanded="false" aria-controls="collapseExample">
+    WARD
+  </button> 
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Content1" aria-expanded="false" aria-controls="collapseExample">
+    BILLING
+  </button> 
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#Content1" aria-expanded="false" aria-controls="collapseExample">
+    ANCILLARY
+  </button> 
+
     </div>
-      <div class="col-sm-5 col-md-7">
-      .col-sm-6 .col-md-8
-      </div>
-      
+    <div class="collapse" id="Content1">
+  <div class="card card-body">
+  <div class="list-group">
+  
+  <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+  
+</div>
+</ul>
   </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
