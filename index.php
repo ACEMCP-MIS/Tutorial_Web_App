7ed00d027c9b450b5e83c8e5b3027d593ffff698
Humphrey Castro <54143005+Preyyy@users.noreply.github.com>
2024-02-01 09:37:50 +0800

    Designing process

--- index.php
+++ index.php
@@ -6,101 +6,147 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ACEMCP - GUIDES</title>
   <link rel="shortcut icon" href="icons/favicon.ico">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
+<header>
+<h1>ACE MEDICAL CENTER PALAWAN SYSTEMS</h2>
 
-  <h1>ACE MEDICAL CENTER PALAWAN SYSTEMS</H1>
-  <p>System Guidlines for ACE EMPLOYEES
-        </p>
+</header>
+<hr>
   <section class="content demo">
+<div class="row">
 
+  
 
-    <div class="card-hghlght demo-card" id="HIS">
+  <div class="col 1">
+  <div class="card-hghlght demo-card" id="HIS">
       <div class="card-body m-5">
-        <h1>HEALTH INFORMATION SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
-        <button class="button-35" role="button" onclick="document.location='HIS/HOME_HIS.php'">Go to Site</button>
+        <div class="text">
+        <h2>HEALTH INFORMATION SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
+        <button class="button-35" role="button" onclick="document.location='HIS/HOME_HIS.php'">Go</button>
+        </div>
+        
+       
 
       </div>
     </div>
+  </div>
 
-
-    <div class="card-hghlght demo-card">
+  <div class="col 2">
+  <div class="card-hghlght demo-card">
       <div class="card-body m-5">
-        <h1>MATERIALS MANAGEMENT SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
+        <h2>MATERIALS MANAGEMENT SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
+        <button class="button-35" role="button">Go</button>
+
+      </div>
+    </div>
+  </div>
+  
+  <div class="col 3">
+  <div class="card-hghlght demo-card">
+      <div class="card-body m-5">
+        <h2>FINANCIAL MANAGEMENT SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
         <button class="button-35" role="button">Go to Site</button>
 
       </div>
     </div>
-    <div class="card-hghlght demo-card">
+  </div>
+
+  <div class="col 4">
+  <div class="card-hghlght demo-card">
       <div class="card-body m-5">
-        <h1>FINANCIAL MANAGEMENT SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
+
+        <h2>PHILHEALTH INFORMATION SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
         <button class="button-35" role="button">Go to Site</button>
 
       </div>
     </div>
-    <div class="card-hghlght demo-card">
-      <div class="card-body m-5">
+  </div>
 
-        <h1>PHILHEALTH INFORMATION SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
+</div>
+
+
+
+   
+  
+
+<div class="row">
+
+<div class="col">
+<div class="card-hghlght demo-card" id="HIS">
+      <div class="card-body m-5">
+        <h2>PRICE UPDATE REQUISITION</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
         <button class="button-35" role="button">Go to Site</button>
 
       </div>
     </div>
+</div>
+
+<div class="col">
+<div class="card-hghlght demo-card">
+      <div class="card-body m-5">
+        <h2>ISSUANCE SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
+        <button class="button-35" role="button">Go to Site</button>
+
+      </div>
+    </div>
+</div>
+<div class="col">
+<div class="card-hghlght demo-card">
+      <div class="card-body m-5">
+        <h2>FINANCIAL MANAGEMENT SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
+        <button class="button-35" role="button">Go to Site</button>
+
+      </div>
+    </div>
+</div>
+<div class="col">
+<div class="card-hghlght demo-card">
+      <div class="card-body m-5">
+
+        <h2>PHILHEALTH INFORMATION SYSTEM</h2>
+        <h6>Click below for Health Information System Guidelines
+        </h6>
+        <button class="button-35" role="button">Go to Site</button>
+
+      </div>
+    </div>
+</div>
+</div>
+
+    
+  
+
+
+
+    
+ 
+
+
+    
   </section>
-  <hr />
-  <section class="content demo">
 
-
-    <div class="card-hghlght demo-card" id="HIS">
-      <div class="card-body m-5">
-        <h1>PRICE UPDATE REQUISITION</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
-        <button class="button-35" role="button">Go to Site</button>
-
-      </div>
-    </div>
-
-
-    <div class="card-hghlght demo-card">
-      <div class="card-body m-5">
-        <h1>ISSUANCE SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
-        <button class="button-35" role="button">Go to Site</button>
-
-      </div>
-    </div>
-    <div class="card-hghlght demo-card">
-      <div class="card-body m-5">
-        <h1>FINANCIAL MANAGEMENT SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
-        <button class="button-35" role="button">Go to Site</button>
-
-      </div>
-    </div>
-    <div class="card-hghlght demo-card">
-      <div class="card-body m-5">
-
-        <h1>PHILHEALTH INFORMATION SYSTEM</h1>
-        <p>Click below for Health Information System Guidelines
-        </p>
-        <button class="button-35" role="button">Go to Site</button>
-
-      </div>
-    </div>
-  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+ 
+  
 </body>
 
 
