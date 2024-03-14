f25819dbe646cfb9d00f8a61362489f0789d59f3
Preyyy <castrohumphrey6@gmail.com>
2024-03-14 15:02:52 +0800

    Content added

---
+++ MMS/script.js
@@ -0,0 +1,5 @@
+const circle = document.querySelectorAll(".circle"),
+Progress_Bar  = document.querySelectorAll(".indicator"),
+buttons = document.querySelectorAll("button");
+
+console.log(circle, Progress_Bar, buttons)
